9f4ed5e2388b5cab957b77c120dcefd731defd2d
Joshua Lee <jminlee24@outlook.com>
2020-11-06 00:17:33 -0500

    yay

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require('express');
 const cors = require('cors');
 const StudentVue = require('studentvue.js')
 const app = express();
+const port = process.env.PORT || 5000
 app.use(cors())
 const url = 'https://md-mcps-psv.edupoint.com/';
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 app.options('*', cors())
 app.use(express.json());
 
@@ -35,6 +42,6 @@ app.post('/done', async (req, res)=>{
     
 })
 
-app.listen(process.env.PORT || 5000, ()=>{
-    console.log(process.env.PORT);
+app.listen(port, ()=>{
+    console.log(port);
 });
