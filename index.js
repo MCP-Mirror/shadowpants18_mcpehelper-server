844ef57027982c51f6e6f598e3e484555e94d8ca
Joshua Lee <jminlee24@outlook.com>
2020-11-06 00:32:32 -0500

    yay

--- index.js
+++ index.js
@@ -5,14 +5,7 @@ const app = express();
 const port = process.env.PORT || 5000
 app.use(cors())
 const url = 'https://md-mcps-psv.edupoint.com/';
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", 'http://mcpsvuehelper.surge.sh/');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
-app.options('*', cors())
+
 app.use(express.json());
 
 app.get('/', (req, res)=>{
